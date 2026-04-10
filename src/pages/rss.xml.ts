00f3147224f324a4ac32d0967f7e548507dc5435
A. Enyichi Agu <enyichiaagu@gmail.com>
2026-04-10 19:41:13 +0100

    a bit of advanced astro

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,12 +1,20 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 import type { APIContext } from "astro";
 
 export async function GET(context: APIContext) {
+	const posts = await getCollection("blog");
+
 	return rss({
 		title: "Astro Learner | Blog",
 		description: "My journey learning Astro",
 		site: `${context.site}`,
-		items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+		items: posts.map(({ data: { title, pubDate, description }, id }) => ({
+			title,
+			pubDate,
+			description,
+			link: `/posts/${id}/`,
+		})),
 		customData: `<language>en-us</language>`,
 	});
 }
